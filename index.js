464b5651cb1759df54c827e74fb0f554d81dc007
LUCY <wangecikariu.luc@gmail.com>
2023-07-16 20:51:09 +0300

    Functions Lab Continued

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // code your solution here
-function saturdayFun(name = "I", activity = "roller-skate") {
-    console.log(`${name} want to ${activity}`);
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
 }
 
-function mondayWork(name = "I", activity = "go to the office") {
-    console.log(`${name} want to ${activity}`);
+const newActivity = "go swimming";
+console.log(saturdayFun(newActivity));
+console.log(saturdayFun());
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
 }
 
-function wrapAdjective() {
-const wrapAdjective = ("*") ("are someone special");
-const wrapAdjective = ("!!")
+const newMonday = "work from home";
+console.log(mondayWork(newMonday));
+console.log(mondayWork());
+
+function wrapAdjective(symbol = "*") {
+    return function(y = "special") {
+        return `You are ${symbol}${y}${symbol}!`;
+    }
 }
